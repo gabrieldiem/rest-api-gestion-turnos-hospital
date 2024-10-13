5a006cdd1837c8d12b9f75906c51f1856212a274
NicoPaez <nicopaez@gmail.com>
2024-10-12 21:54:02 -0300

    refactor: agregado del sistema

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'sinatra/json'
 require 'sequel'
 require 'sinatra/custom_logger'
 require_relative './config/configuration'
@@ -6,36 +7,45 @@ require_relative './lib/version'
 Dir[File.join(__dir__, 'dominio', '*.rb')].each { |file| require file }
 Dir[File.join(__dir__, 'persistencia', '*.rb')].each { |file| require file }
 
-customer_logger = Configuration.logger
-DB = Configuration.db
-DB.loggers << customer_logger
-set :logger, customer_logger
-set :default_content_type, :json
-set :environment, ENV['APP_MODE'].to_sym
+configure do
+  customer_logger = Configuration.logger
+  DB = Configuration.db # rubocop:disable  Lint/ConstantDefinitionInBlock
+  DB.loggers << customer_logger
+  set :logger, customer_logger
+  set :default_content_type, :json
+  set :environment, ENV['APP_MODE'].to_sym
+  set :sistema, Sistema.new(RepositorioUsuarios.new)
+end
+
+before do
+  if !request.body.nil? && request.body.size.positive?
+    request.body.rewind
+    @params = JSON.parse(request.body.read, symbolize_names: true)
+  end
+end
+
+def sistema
+  settings.sistema
+end
 
 get '/version' do
-  { version: Version.current }.to_json
+  json({ version: Version.current })
 end
 
 post '/reset' do
-  RepositorioUsuarios.new.delete_all
   status 200
 end
 
 get '/usuarios' do
-  usuarios = RepositorioUsuarios.new.all
+  usuarios = sistema.usuarios
   respuesta = []
   usuarios.map { |u| respuesta << { email: u.email, id: u.id } }
   status 200
-  respuesta.to_json
+  json(respuesta)
 end
 
 post '/usuarios' do
-  @body ||= request.body.read
-  parametros_usuario = JSON.parse(@body)
-
-  usuario = Usuario.new(parametros_usuario['email'])
-  RepositorioUsuarios.new.save(usuario)
+  usuario = sistema.crear_usuario(@params['email'])
   status 201
   { id: usuario.id, email: usuario.email }.to_json
 end
