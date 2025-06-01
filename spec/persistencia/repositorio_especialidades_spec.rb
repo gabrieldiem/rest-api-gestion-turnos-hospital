b7057c8077c15c9ee087e0fe724af45a841374c2
sev <santisevitz@hotmail.com>
2025-06-01 12:21:57 -0300

    unit-test-1.0: agregando unit test para especialidad respositorio

--- spec/persistencia/repositorio_especialidades_spec.rb
+++ spec/persistencia/repositorio_especialidades_spec.rb
@@ -3,9 +3,17 @@ require_relative '../../dominio/especialidad'
 require_relative '../../persistencia/repositorio_especialidades'
 
 describe RepositorioEspecialidades do
-  it 'deberia guardar y asignar id si el usuario es nuevo' do
+  it 'deberia guardar y asignar id si la especialidad es nuevo' do
     especialidad = Especialidad.new('Cardiología', 30, 5, 'card')
     described_class.new.save(especialidad)
     expect(especialidad.id).not_to be_nil
+  end
+
+  it 'deberia recuperar todos' do
+    repositorio = described_class.new
+    cantidad_de_especialidades_iniciales = repositorio.all.size
+    especialidad = Especialidad.new('Cardiología', 30, 5, 'card')
+    repositorio.save(especialidad)
+    expect(repositorio.all.size).to be(cantidad_de_especialidades_iniciales + 1)
   end
 end
