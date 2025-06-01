8f2d71e61e672a32cefd0d8f78bcf9e4ba6168c9
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-06-01 14:41:23 -0300

    refactor: pasamos el objeto especialidad al medico

--- features/abm_medicos.feature
+++ features/abm_medicos.feature
@@ -10,6 +10,6 @@ Característica: Alta de un médico en el sistema HMS
 
   @wip @indev
   Escenario: 2.0.1 - Se puede dar de alta un médico correctamente
-    Dado que ingreso el nombre "Juan", apellido "Pérez", matrícula "NAC123" y especialidad "card" para el médico
+    Dado que ingreso el nombre "Juan", apellido "Pérez", matrícula "NAC123" y especialidad "Cardiología" para el médico
     Cuando doy de alta al medico
     Entonces el médico se registra exitosamente
--- spec/modelo/medico_spec.rb
+++ spec/modelo/medico_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 require_relative '../../dominio/medico'
+require_relative '../../dominio/especialidad'
 
 describe Medico do
-  it 'se crea exitosamente con nombre Juan, apellido Pérez, matrícula NAC123 y especialidad card' do
-    medico = described_class.new('Juan', 'Pérez', 'NAC123', 'card')
-    expect(medico).to have_attributes(nombre: 'Juan', apellido: 'Pérez', matricula: 'NAC123', especialidad: 'card')
+  it 'se crea exitosamente con nombre Juan, apellido Pérez, matrícula NAC123 y especialidad Cardiología' do
+    especialidad = Especialidad.new('Cardiología', 30, 5, 'card')
+    medico = described_class.new('Juan', 'Pérez', 'NAC123', especialidad)
+    expect(medico).to have_attributes(nombre: 'Juan', apellido: 'Pérez', matricula: 'NAC123', especialidad:)
   end
 end
