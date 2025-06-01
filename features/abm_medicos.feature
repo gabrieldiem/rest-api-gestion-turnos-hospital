e9ea96ea77b0dcb8a6b544a92f39c975baa01a43
saintttt <alivetheoldschool@hotmail.com>
2025-06-01 14:04:52 -0300

    acceptance-test-2.0: agregado de pruebas de aceptacion a testear

---
+++ features/abm_medicos.feature
@@ -0,0 +1,15 @@
+# language: es
+Característica: Alta de un médico en el sistema HMS
+  Como administrador de HMS
+  Quiero dar de alta un médico
+
+  Antecedentes:
+    Dado que existe la especialidad "Cardiología" con código "card" 
+    Y que existe la especialidad "Pediatría" con código "pedi" 
+    Y que existe la especialidad "Neurología" con código "neur"
+
+  @wip
+  Escenario: 2.0.1 - Se puede dar de alta un médico correctamente
+    Dado que ingreso el nombre "Juan", apellido "Pérez", matrícula "NAC123" y especialidad "card" para el médico
+    Cuando doy de alta al medico
+    Entonces el médico se registra exitosamente
---
+++ features/step_definitions/medicos_steps.rb
@@ -0,0 +1,15 @@
+Dado('que existen las especialidades {string} con código {string}') do |_especialidad, _codigo|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('que ingreso el nombre {string}, apellido {string}, matrícula {string} y especialidad {string} para el médico') do |_string, _string2, _string3, _string4|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Cuando('doy de alta al medico') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Entonces('el médico se registra exitosamente') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
