24dd96c6da8db7ae58a6008cf6830c7b7f2588e6
gabrieldiem <gabriel.diem5@gmail.com>
2025-06-01 10:47:03 -0300

    acceptance-test-1.0.1: agrego gherking

---
+++ features/abm_especialidades.feature
@@ -0,0 +1,13 @@
+# language: es
+Característica: Alta de especialidades en HMS
+  Como administrador de HMS
+  Quiero dar de alta especialidades
+
+  @wip
+  Escenario: 1.0.1 - Se puede dar de alta una especialidad correctamente
+    Dado que completo el nombre con "Cardiología"
+    Y que completo la duración de turnos con "30" minutos
+    Y que completo la recurrencia máxima con "5"
+    Y que completo el código con "card"
+    Cuando doy de alta la especialidad
+    Entonces la especialidad se crea exitosamente
---
+++ features/step_definitions/especialidades_steps.rb
@@ -0,0 +1,23 @@
+Dado('que completo el nombre con {string}') do |_nombre_especialidad|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('que completo la duración de turnos con {string} minutos') do |_duracion_de_turno|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('que completo la recurrencia máxima con {string}') do |_recurrencia_maxima|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('que completo el código con {string}') do |_codigo|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Cuando('doy de alta la especialidad') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Entonces('la especialidad se crea exitosamente') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
