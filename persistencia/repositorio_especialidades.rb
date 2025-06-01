c7f74d632ebf74d5cc1708d6ff5bb02c5ea98afb
Aaron Sanjines <asanjines@fi.uba.ar>
2025-06-01 16:59:07 -0300

    unit-test-2.0: agregado de test para chequear que la especialidad devuelta de la DB no tenga un created_on null

--- persistencia/repositorio_especialidades.rb
+++ persistencia/repositorio_especialidades.rb
@@ -7,7 +7,9 @@ class RepositorioEspecialidades < AbstractRepository
   protected
 
   def load_object(a_hash)
-    Especialidad.new(a_hash[:nombre], a_hash[:duracion], a_hash[:recurrencia_maxima], a_hash[:codigo], a_hash[:id])
+    especialidad = Especialidad.new(a_hash[:nombre], a_hash[:duracion], a_hash[:recurrencia_maxima], a_hash[:codigo], a_hash[:id])
+    especialidad.created_on = a_hash[:created_on]
+    especialidad
   end
 
   def changeset(especialidad)
--- spec/persistencia/repositorio_especialidades_spec.rb
+++ spec/persistencia/repositorio_especialidades_spec.rb
@@ -16,4 +16,12 @@ describe RepositorioEspecialidades do
     repositorio.save(especialidad)
     expect(repositorio.all.size).to be(cantidad_de_especialidades_iniciales + 1)
   end
+
+  it 'la especialidad devuelta no puede ser nil' do
+    repositorio = described_class.new
+    especialidad = Especialidad.new('Cardiología', 30, 5, 'card')
+    repositorio.save(especialidad)
+    especialidad_recuperada = repositorio.find(especialidad.id)
+    expect(especialidad_recuperada.created_on).not_to be_nil
+  end
 end
