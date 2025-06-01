7fc63f752dd165135ea2c23dce21586fb6cc7bc1
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-06-01 16:04:57 -0300

    unit-test-2.0: Médicos: crea un médico nuevo

--- spec/modelo/turnero_spec.rb
+++ spec/modelo/turnero_spec.rb
@@ -8,16 +8,26 @@ describe Turnero do
   let(:repositorio_especialidades) { RepositorioEspecialidades.new }
   let(:turnero) { described_class.new(RepositorioUsuarios.new, repositorio_especialidades) }
 
-  it 'crea una especialidad nuevo' do
-    especialidad_nuevo = turnero.crear_especialidad('Cardiología', 30, 5, 'card')
-    expect(especialidad_nuevo).to have_attributes(nombre: 'Cardiología', duracion: 30, recurrencia_maxima: 5, codigo: 'card')
+  describe 'Especialidades' do
+    it 'crea una especialidad nuevo' do
+      especialidad_nuevo = turnero.crear_especialidad('Cardiología', 30, 5, 'card')
+      expect(especialidad_nuevo).to have_attributes(nombre: 'Cardiología', duracion: 30, recurrencia_maxima: 5, codigo: 'card')
+    end
+
+    it 'crea una especialidad nuevo y lo guarda en el repositorio' do
+      especialidad_nuevo = turnero.crear_especialidad('Cardiología', 30, 5, 'card')
+      especialidad_guardada = repositorio_especialidades.all.first
+      expect(repositorio_especialidades.all.size).to eq(1)
+      expect(especialidad_guardada).to have_attributes(nombre: especialidad_nuevo.nombre, duracion: especialidad_nuevo.duracion, recurrencia_maxima: especialidad_nuevo.recurrencia_maxima,
+                                                       codigo: especialidad_nuevo.codigo)
+    end
   end
 
-  it 'crea una especialidad nuevo y lo guarda en el repositorio' do
-    especialidad_nuevo = turnero.crear_especialidad('Cardiología', 30, 5, 'card')
-    especialidad_guardada = repositorio_especialidades.all.first
-    expect(repositorio_especialidades.all.size).to eq(1)
-    expect(especialidad_guardada).to have_attributes(nombre: especialidad_nuevo.nombre, duracion: especialidad_nuevo.duracion, recurrencia_maxima: especialidad_nuevo.recurrencia_maxima,
-                                                     codigo: especialidad_nuevo.codigo)
+  describe 'Médicos' do
+    xit 'crea un médico nuevo' do
+      especialidad = turnero.crear_especialidad('Cardiología', 30, 5, 'card')
+      medico_nuevo = turnero.crear_medico('Juan', 'Pérez', 'NAC123', especialidad)
+      expect(medico_nuevo).to have_attributes(nombre: 'Juan', apellido: 'Pérez', matricula: 'NAC123', especialidad:)
+    end
   end
 end
