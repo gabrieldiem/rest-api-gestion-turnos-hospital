3948424126b3a1a51e2890825f9574b345e76bd1
Aaron Sanjines <asanjines@fi.uba.ar>
2025-06-02 19:00:10 -0300

    acceptance-test-5.0: commit inicial con los gherkin y los steps a definir para la US 5.0

---
+++ features/step_definitions/reserva_turnos_steps.rb
@@ -0,0 +1,27 @@
+Dado('que existe un doctor de apellido {string} registrado con matrícula {string}') do |_string, _string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('que hay un paciente registrado con username {string}') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('el Dr. {string} tiene un turno disponible el {string} a las {string}') do |_string, _string2, _string3|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Cuando('reservo el turno con el médico de matrícula {string} en la fecha {string} y la hora {string}') do |_string, _string2, _string3|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Entonces('recibo el mensaje {string}') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Cuando('reservo el turno con el médico de matrícula {string} en la fecha {string} y la hora {string} con el username {string}') do |_string, _string2, _string3, _string4|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('el Dr. {string} tenía un turno disponible el {string} a las {string} y alguien más lo reservó') do |_string, _string2, _string3|
+  pending # Write code here that turns the phrase above into concrete actions
+end
