aab82038dc355cb93e7744575f066bc466be9046
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-06-02 14:39:32 -0300

    unit-test-4.0.1: Medico obtenemos los turnos disponibles del médico

--- spec/modelo/medico_spec.rb
+++ spec/modelo/medico_spec.rb
@@ -3,9 +3,25 @@ require_relative '../../dominio/medico'
 require_relative '../../dominio/especialidad'
 
 describe Medico do
+  let(:especialidad) { Especialidad.new('Cardiología', 30, 5, 'card') }
+
   it 'se crea exitosamente con nombre Juan, apellido Pérez, matrícula NAC123 y especialidad Cardiología' do
-    especialidad = Especialidad.new('Cardiología', 30, 5, 'card')
     medico = described_class.new('Juan', 'Pérez', 'NAC123', especialidad)
     expect(medico).to have_attributes(nombre: 'Juan', apellido: 'Pérez', matricula: 'NAC123', especialidad:)
+  end
+
+  xit 'obtenemos los turnos disponibles del médico' do
+    allow(Date).to receive(:today).and_return(Date.new(2025, 10, 6))
+    medico = described_class.new('Juan', 'Pérez', 'NAC123', :especialidad)
+
+    turnos_disponibles = medico.obtener_turnos_disponibles
+
+    expect(turnos_disponibles).to eq([
+                                       { 'fecha' => '2025-11-06', 'hora' => '08:00' },
+                                       { 'fecha' => '2025-11-06', 'hora' => '08:30' },
+                                       { 'fecha' => '2025-11-06', 'hora' => '09:00' },
+                                       { 'fecha' => '2025-11-06', 'hora' => '09:30' },
+                                       { 'fecha' => '2025-11-06', 'hora' => '10:00' }
+                                     ])
   end
 end
