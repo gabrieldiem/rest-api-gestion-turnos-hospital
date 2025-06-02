28ca575dfafaafee05eb6d18f315012e3503f55a
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-06-02 13:53:17 -0300

    acceptence-test-4.0: definiendo steps faltantes y cambiando otros

--- features/step_definitions/turnos_por_medico_steps.rb
+++ features/step_definitions/turnos_por_medico_steps.rb
@@ -1,34 +1,37 @@
-Dado('que esta registrado el username {string}') do |_string|
+Dado('que esta registrado el username {string}') do |username|
   @repo_pacientes = RepostiorioPacientes.new
   @repo_pacientes.delete_all
+  @username_registrado = username
   registered_body = { email: 'juan.perez@example.com', dni: '42951753', username: }.to_json
   @response = Faraday.post('/pacientes', registered_body, { 'Content-Type' => 'application/json' })
   expect(@response.status).to eq(201)
 end
 
-Dado('que existe un médico con nombre {string}, apellido {string}, matrícula {string} y especialidad {string} con duración de {string} minutos') do |nombre, apellido, matricula, codigo, duracion|
+Dado('que existe un médico con nombre {string}, apellido {string}, matrícula {string} y especialidad {string} con duración de {string} minutos') do |nombre, apellido, matricula, especial, duracion|
   RepostiorioEspecialidades.new.delete_all
   RepostiorioMedicos.new.delete_all
 
-  especialidad_body = { nombre: _codigo, duracion:, recurrencia_maxima: 5, codigo: codigo.downcase[0..3] }.to_json
+  especialidad_body = { nombre: especial, duracion:, recurrencia_maxima: 5, codigo: especial.downcase[0..3] }.to_json
   @response = Faraday.post('/especialidades', especialidad_body, { 'Content-Type' => 'application/json' })
   expect(@response.status).to eq(201)
 
-  medicos_body = { nombre:, apellido:, matricula:, especialidad: especialidad.downcase[0..3] }.to_json
+  medicos_body = { nombre:, apellido:, matricula:, especialidad: especial.downcase[0..3] }.to_json
   @response = Faraday.post('/medicos', medicos_body, { 'Content-Type' => 'application/json' })
   expect(@response.status).to eq(201)
 end
 
-Dado('que hago una consulta con el username {string}') do |_string|
+Dado('que hago una consulta con el username {string}') do |username|
+  @username_solicitante = username
 end
 
 Dado('no hay turnos asignados para el médico con matrícula {string}') do |matricula|
-  expect(parsed_response['medico']['matricula']).to matricula
+  @matricula_medico = matricula
+  expect(parsed_response['medico']['matricula']).to eq(matricula)
   expect(parsed_response['turnos']).to be_empty
 end
 
 Cuando('solicito los turnos disponibles con {string}') do |matricula|
-  @response = Faraday.get("/medicos/#{matricula}/turnos-disponibles")
+  @response = Faraday.get("/medicos/#{matricula}/turnos-disponibles?username=#{@username_solicitante}")
   @parsed_response = JSON.parse(@response.body)
   expect(@response.status).to eq(200)
 end
@@ -39,16 +42,23 @@ Entonces('recibo los próximos {string} turnos disponibles') do |string|
   expect(@turnos_disponibles.size).to eq(string.to_i)
 end
 
-Entonces('son del {string} a las {string} en adelante') do |_fecha, _hora|
-  pending
+Entonces('son del {string} a las {string} en adelante') do |fecha, hora|
+  proximo_turno = @turnos_disponibles.first
+  expect(proximo_turno['fecha']).to eq(fecha)
+  expect(proximo_turno['hora']).to eq(hora)
 end
 
-Dado('el médico con matrícula {string} tiene un turno asignado el {string} {string}') do |_matricula, _fecha, _hora|
-  pending # Write code here that turns the phrase above into concrete actions
+Dado('el médico con matrícula {string} tiene un turno asignado el {string} {string}') do |matricula, _fecha, _hora|
+  medico = RepostiorioMedicos.new.find_by_matricula(matricula)
+  medico.asignar_turno(fecha, hora, "Juan Perez")
+  expect(medico['matricula']).to eq(matricula)
 end
 
 Entonces('no se muestran turnos disponibles') do
-  pending # Write code here that turns the phrase above into concrete actions
+  response = Faraday.get("/medicos/#{matricula}/turnos-disponibles?username=#{@username_solicitante}")
+  parsed_response = JSON.parse(response.body)
+  expect(response.status).to eq(200)
+  expect(parsed_response['turnos']).to be_empty
 end
 
 Dado('que hoy es {string}') do |fecha|
@@ -56,6 +66,7 @@ Dado('que hoy es {string}') do |fecha|
   allow(Date).to receive(:today).and_return(@fecha_actual)
 end
 
-Dado('el médico con matrícula {string} no tiene turnos disponibles en los próximos {int} días') do |_matricula, _dias|
-  pending # Write code here that turns the phrase above into concrete actions
+Dado('el médico con matrícula {string} no tiene turnos disponibles en los próximos {int} días') do |matricula, dias|
+  medico = RepostiorioMedicos.new.find_by_matricula(matricula)
+  expect{ medico.tiene_turnos? }.to be false
 end
