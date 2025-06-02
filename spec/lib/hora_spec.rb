e48d2a550847dbc2862a577338328e73c7fe802d
gabrieldiem <gabriel.diem5@gmail.com>
2025-06-02 18:38:12 -0300

    unit-test-4.0: agrego unit test a la hora

---
+++ spec/lib/hora_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require_relative '../../lib/hora'
+
+describe Hora do
+  it 'Una hora que es 10h 31min se crea con 10h 31min' do
+    hora = described_class.new(10, 31)
+
+    expect(hora.is_a?(described_class)).to be true
+    expect(hora).to have_attributes(hora: 10, minutos: 31)
+  end
+end
