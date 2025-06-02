688cd2a417a1c9ca490be5c9e03e5676b49c7d8e
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-06-02 15:31:50 -0300

    refactor-4.0.1: Medicos obtenemos los turnos disponibles del médico

--- dominio/medico.rb
+++ dominio/medico.rb
@@ -18,17 +18,23 @@ class Medico
   private
 
   def calcular_turnos_disponibles(fecha_turno, duracion_turno)
-    hora_inicio = 8
     cantidad_de_turnos = 0
     max_cantidad_de_turnos = 5
     turnos_disponibles = []
 
     while cantidad_de_turnos < max_cantidad_de_turnos
-      hora_turno = DateTime.new(fecha_turno.year, fecha_turno.month, fecha_turno.day, hora_inicio + (cantidad_de_turnos * duracion_turno / 60), (cantidad_de_turnos * duracion_turno) % 60)
+      hora_turno = calcular_fecha_siguiente(fecha_turno, cantidad_de_turnos, duracion_turno)
       turnos_disponibles << { 'fecha' => fecha_turno.strftime('%d-%m-%Y'), 'hora' => hora_turno.strftime('%H:%M') }
       cantidad_de_turnos += 1
     end
 
     turnos_disponibles
+  end
+
+  def calcular_fecha_siguiente(fecha_actual, turno_siguiente, duracion)
+    hora_inicio = 8
+    hora_siguiente = hora_inicio + (turno_siguiente * duracion / 60)
+    minutos_siguiente = (turno_siguiente * duracion) % 60
+    DateTime.new(fecha_actual.year, fecha_actual.month, fecha_actual.day, hora_siguiente, minutos_siguiente)
   end
 end
