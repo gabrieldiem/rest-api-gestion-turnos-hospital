a6b9644a1c8398c122da33e1eb3759bad5cdf6bf
gabrieldiem <gabriel.diem5@gmail.com>
2025-06-02 19:34:07 -0300

    unit-test-4.0: agrego test de horario igual a otro horario

--- spec/lib/horario_spec.rb
+++ spec/lib/horario_spec.rb
@@ -11,4 +11,12 @@ describe Horario do
     expect(horario.is_a?(described_class)).to be true
     expect(horario).to have_attributes(fecha:, hora:)
   end
+
+  it 'Un horario que tiene hora 10h 31min y fecha 25/10/1999 es igual que otro con los mismo datos' do
+    hora = Hora.new(10, 31)
+    fecha = Date.parse('25/10/1999')
+    horario = described_class.new(fecha, hora)
+
+    expect(horario).to eq described_class.new(fecha, hora)
+  end
 end
