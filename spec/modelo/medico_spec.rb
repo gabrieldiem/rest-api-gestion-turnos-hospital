4c927c8ce7ab80efcc720747ce905d8df818c10d
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-06-02 20:59:23 -0300

    delete-4.0: Borramos tests de calcular turnos disponibles porque ahora esta en CalculadorDeTurnos

--- spec/modelo/medico_spec.rb
+++ spec/modelo/medico_spec.rb
@@ -9,33 +9,4 @@ describe Medico do
     medico = described_class.new('Juan', 'Pérez', 'NAC123', especialidad)
     expect(medico).to have_attributes(nombre: 'Juan', apellido: 'Pérez', matricula: 'NAC123', especialidad:)
   end
-
-  xit 'obtenemos los turnos disponibles del médico' do
-    medico = described_class.new('Juan', 'Pérez', 'NAC123', especialidad)
-
-    turnos_disponibles = medico.obtener_turnos_disponibles(Date.new(2025, 6, 10))
-
-    expect(turnos_disponibles).to eq([
-                                       { 'fecha' => '11/06/2025', 'hora' => '8:00' },
-                                       { 'fecha' => '11/06/2025', 'hora' => '8:30' },
-                                       { 'fecha' => '11/06/2025', 'hora' => '9:00' },
-                                       { 'fecha' => '11/06/2025', 'hora' => '9:30' },
-                                       { 'fecha' => '11/06/2025', 'hora' => '10:00' }
-                                     ])
-  end
-
-  xit 'obtenemos los turnos, pero a partir de las 8:15 porque tiene un turno asignado' do
-    medico = described_class.new('Juan', 'Pérez', 'NAC123', especialidad)
-
-    medico.asignar_turno(Date.new(2025, 6, 11), '08:00', 'Paciente A')
-    turnos_disponibles = medico.obtener_turnos_disponibles(Date.new(2025, 6, 10))
-
-    expect(turnos_disponibles).to eq([
-                                       { 'fecha' => '11/06/2025', 'hora' => '8:30' },
-                                       { 'fecha' => '11/06/2025', 'hora' => '9:00' },
-                                       { 'fecha' => '11/06/2025', 'hora' => '9:30' },
-                                       { 'fecha' => '11/06/2025', 'hora' => '10:00' },
-                                       { 'fecha' => '11/06/2025', 'hora' => '10:30' }
-                                     ])
-  end
 end
