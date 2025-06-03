71fd720ab3438fbdd2ed3be4894b387e753fa97a
gabrieldiem <gabriel.diem5@gmail.com>
2025-06-03 10:41:52 -0300

    refactor-test-4.0: pulo tests de aceptacion, escenario y implementacion

--- features/abm_turnos_por_medico.feature
+++ features/abm_turnos_por_medico.feature
@@ -4,35 +4,31 @@ Característica: Consultar turnos disponibles por médico
   Quiero consultar los turnos disponibles de un médico con una determinada matrícula
 
   Antecedentes:
-    Dado que esta registrado el username "juanperez"
+    Dado que esta registrado el username "juanperez" y DNI "42951753'"
     Y que existe un médico con nombre "Juan", apellido "Perez", matrícula "NAC123" y especialidad "Traumatología" con duración de "15" minutos
 
   Escenario: 4.0.1 - Se puede consultar turnos disponibles de un médico sin turnos asignados
-    Dado que hago una consulta con el username "juanperez"
-    Y que hoy es "20/05/2025"
+    Dado que hoy es "20/05/2025"
     Y no hay turnos asignados para el médico con matrícula "NAC123"
     Cuando solicito los turnos disponibles con "NAC123"
-    Entonces recibo los próximos "5" turnos disponibles
+    Entonces recibo los próximos 5 turnos disponibles
     Y son del "21/05/2025" a las "8:00" en adelante
 
   Escenario: 4.0.2 - Se puede consultar turnos disponibles de un médico con un turno asignado
-    Dado que hago una consulta con el username "juanperez"
-    Y que hoy es "20/05/2025"
+    Dado que hoy es "20/05/2025"
     Y el médico con matrícula "NAC123" tiene un turno asignado el "21/05/2025" "8:00"
     Cuando solicito los turnos disponibles con "NAC123"
-    Entonces recibo los próximos "5" turnos disponibles
+    Entonces recibo los próximos 5 turnos disponibles
     Y son del "21/05/2025" a las "8:15" en adelante
 
   Escenario: 4.0.3 - Consultar turnos de un médico que no existe no muestra turnos
-    Dado que hago una consulta con el username "juanperez"
     Cuando solicito los turnos disponibles con "ABC000"
     Entonces no se muestran turnos disponibles
     Y recibo un mensaje de error "No existe un médico con la matrícula ABC000"
 
   @wip
   Escenario: 4.0.4 - Se puede consultar turnos de un médico sin turnos disponibles en los próximos 40 días
-    Dado que hago una consulta con el username "juanperez"
-    Y que hoy es "20/05/2025"
+    Dado que hoy es "20/05/2025"
     Y el médico con matrícula "NAC123" no tiene turnos disponibles en los próximos 40 días
     Cuando solicito los turnos disponibles con "NAC123"
     Entonces no se muestran turnos disponibles
@@ -40,7 +36,6 @@ Característica: Consultar turnos disponibles por médico
 
   @wip
   Escenario: 4.0.5 - No se pueden consultar turnos sin estar registrado
-    Dado que hago una consulta con el username "santisev"
     Cuando solicito los turnos disponibles con "NAC123"
     Entonces no se muestran turnos disponibles
     Y recibo un mensaje de error "Para consultar turnos tiene que estar registrado"
--- features/step_definitions/turnos_por_medico_steps.rb
+++ features/step_definitions/turnos_por_medico_steps.rb
@@ -8,33 +8,42 @@ Before do
   RepositorioEspecialidades.new(@logger).delete_all
 end
 
-Dado('que esta registrado el username {string}') do |username|
+Dado('que esta registrado el username {string} y DNI {string}') do |username, dni|
   @username_registrado = username
-  @dni_registrado = '42951753'
-  registered_body = { email: 'juan.perez@example.com', dni: @dni_registrado, username: }.to_json
-  @response = Faraday.post('/pacientes', registered_body, { 'Content-Type' => 'application/json' })
+  @dni_registrado = dni
+  body = { email: 'juan.perez@example.com', dni: @dni_registrado, username: }
+  @response = Faraday.post('/pacientes', body.to_json, { 'Content-Type' => 'application/json' })
   expect(@response.status).to eq(201)
 end
 
-Dado('que existe un médico con nombre {string}, apellido {string}, matrícula {string} y especialidad {string} con duración de {string} minutos') do |nombre, apellido, matricula, especial, duracion|
-  RepositorioMedicos.new(@logger).delete_all
-  RepositorioEspecialidades.new(@logger).delete_all
-
-  especialidad_body = { nombre: especial, duracion:, recurrencia_maxima: 5, codigo: especial.downcase[0..3] }.to_json
-  @response = Faraday.post('/especialidades', especialidad_body, { 'Content-Type' => 'application/json' })
+Dado('que existe un médico con nombre {string}, apellido {string}, matrícula {string} y especialidad {string} con duración de {string} minutos') do
+|nombre_medico, apellido_medico, matricula_medico, nombre_especialidad, duracion_turno|
+  codigo_especialidad = nombre_especialidad.downcase[0..3]
+  body = {
+    nombre: nombre_especialidad,
+    duracion: duracion_turno,
+    recurrencia_maxima: 5,
+    codigo: codigo_especialidad
+  }
+  @response = Faraday.post('/especialidades', body.to_json, { 'Content-Type' => 'application/json' })
   expect(@response.status).to eq(201)
 
-  medicos_body = { nombre:, apellido:, matricula:, especialidad: especial.downcase[0..3] }.to_json
-  @response = Faraday.post('/medicos', medicos_body, { 'Content-Type' => 'application/json' })
+  body = {
+    nombre: nombre_medico,
+    apellido: apellido_medico,
+    matricula: matricula_medico,
+    especialidad: codigo_especialidad
+  }
+  @response = Faraday.post('/medicos', body.to_json, { 'Content-Type' => 'application/json' })
   expect(@response.status).to eq(201)
 end
 
-Dado('que hago una consulta con el username {string}') do |username|
-  @username_solicitante = username
-end
+# Dado('que hago una consulta con el username {string}') do |username|
+#   @username_solicitante = username
+# end
 
 Dado('no hay turnos asignados para el médico con matrícula {string}') do |matricula|
-  @matricula_medico = matricula
+  # Nada que hacer
 end
 
 Cuando('solicito los turnos disponibles con {string}') do |matricula|
@@ -43,19 +52,19 @@ Cuando('solicito los turnos disponibles con {string}') do |matricula|
   @parsed_response = JSON.parse(@response.body)
 end
 
-Entonces('recibo los próximos {string} turnos disponibles') do |string|
+Entonces('recibo los próximos {int} turnos disponibles') do |cantidad_de_turnos_esperada|
   @turnos_disponibles = @parsed_response['turnos']
 
-  expect(@turnos_disponibles).not_to be_empty
-  expect(@turnos_disponibles.size).to eq(string.to_i)
+  expect(@turnos_disponibles).not_to be nil
+  expect(@turnos_disponibles.size).to eq(cantidad_de_turnos_esperada)
 end
 
 Entonces('son del {string} a las {string} en adelante') do |fecha, hora|
   fecha = Date.parse(fecha).to_s
 
-  proximo_turno = @turnos_disponibles.first
-  expect(proximo_turno['fecha']).to eq(fecha)
-  expect(proximo_turno['hora']).to eq(hora)
+  primer_turno = @turnos_disponibles.first
+  expect(primer_turno['fecha']).to eq(fecha)
+  expect(primer_turno['hora']).to eq(hora)
 end
 
 Dado('el médico con matrícula {string} tiene un turno asignado el {string} {string}') do |matricula, fecha, hora|
@@ -84,8 +93,8 @@ Entonces('no se muestran turnos disponibles') do
 end
 
 Dado('que hoy es {string}') do |fecha|
-  @fecha_actual = Date.strptime(fecha, '%d/%m/%Y')
-  allow(Date).to receive(:today).and_return(@fecha_actual)
+  @fecha_de_hoy = Date.parse(fecha)
+  allow(Date).to receive(:today).and_return(@fecha_de_hoy)
 end
 
 Dado('el médico con matrícula {string} no tiene turnos disponibles en los próximos {int} días') do |matricula, _dias|
