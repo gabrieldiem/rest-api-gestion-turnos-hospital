cd664a605c40f8ed3ff1ab3480fe0cd59a6c3c22
gabrieldiem <gabriel.diem5@gmail.com>
2025-06-03 01:49:03 -0300

    unit-test-4.0: calcular de turnos maneja caso de 1 turno asignado

--- spec/modelo/calculador_de_turnos_libres_spec.rb
+++ spec/modelo/calculador_de_turnos_libres_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'integration_helper'
 
 require_relative '../../dominio/turnero'
 require_relative '../../dominio/especialidad'
@@ -16,6 +16,8 @@ describe CalculadorDeTurnosLibres do
   end
   let(:repositorio_especialidades) { RepositorioEspecialidades.new(logger) }
   let(:repositorio_medicos) { RepositorioMedicos.new(logger) }
+  let(:repositorio_pacientes) { RepositorioPacientes.new(logger) }
+  let(:repositorio_turnos) { RepositorioTurnos.new(logger) }
   let(:fecha_de_hoy) { Date.new(2025, 6, 10) }
   let(:proveedor_de_fecha) do
     proveedor_double = class_double(Date, today: fecha_de_hoy)
@@ -38,6 +40,9 @@ describe CalculadorDeTurnosLibres do
     medico
   end
 
+  let(:paciente) do
+  end
+
   it 'obtener turnos disponibles de un médico' do
     fecha_de_maniana = fecha_de_hoy + 1
     calculador_de_turnos_libres = described_class.new(Hora.new(8, 0),
@@ -52,5 +57,26 @@ describe CalculadorDeTurnosLibres do
                           Horario.new(fecha_de_maniana, Hora.new(9, 0)),
                           Horario.new(fecha_de_maniana, Hora.new(9, 30)),
                           Horario.new(fecha_de_maniana, Hora.new(10, 0))])
+  end
+
+  it 'obtener turnos disponibles dado que ya se asigno un turno' do
+    paciente = Paciente.new('j@perez.com', '999999999', 'juanperez')
+    repositorio_pacientes.save paciente
+
+    calculador_de_turnos_libres = described_class.new(Hora.new(8, 0),
+                                                      Hora.new(18, 0),
+                                                      proveedor_de_fecha,
+                                                      proveedor_de_hora)
+    fecha_de_maniana = fecha_de_hoy + 1
+
+    turno = medico.asignar_turno(Horario.new(fecha_de_maniana, Hora.new(8, 0)), paciente)
+    repositorio_turnos.save turno
+
+    turnos = calculador_de_turnos_libres.calcular_turnos_disponibles_por_medico medico
+    expect(turnos).to eq([Horario.new(fecha_de_maniana, Hora.new(8, 30)),
+                          Horario.new(fecha_de_maniana, Hora.new(9, 0)),
+                          Horario.new(fecha_de_maniana, Hora.new(9, 30)),
+                          Horario.new(fecha_de_maniana, Hora.new(10, 0)),
+                          Horario.new(fecha_de_maniana, Hora.new(10, 30))])
   end
 end
