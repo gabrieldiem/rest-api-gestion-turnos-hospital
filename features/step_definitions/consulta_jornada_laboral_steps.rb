e03f5e237476931cc11879fe786035b35c35de8b
Aaron Sanjines <asanjines@fi.uba.ar>
2025-06-04 15:04:32 -0300

    acceptance-test-7.0: declaracion de Gherkins y steps para feature de consulta de jornada laboral

---
+++ features/step_definitions/consulta_jornada_laboral_steps.rb
@@ -0,0 +1,39 @@
+Dado('que existe un medico registrado llamado {string} con matricula {string}') do |_string, _string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('que tiene un turno el día {string} a las {string} para {string} con DNI {string} con el medico {string}') do |_string, _string2, _string3, _string4, _string5|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('tiene un turno el día {string} a las {string} para {string} con DNI {string} con el medico {string}') do |_string, _string2, _string3, _string4, _string5|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Cuando('consulto la jornada del profesional con matricula {string}') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Entonces('el sistema debe mostrar que hay {string} turnos asignados en total') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Entonces('debe listar el turno del {string} a las {string} para {string} con DNI {string}') do |_string, _string2, _string3, _string4|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Cuando('consulto la jornada del medico con matricula {string}') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('que no tiene ningún turno asignado') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Entonces('el sistema debe indicar que hay {string} turnos asignados') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Entonces('debe mostrar el mensaje {string}') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
