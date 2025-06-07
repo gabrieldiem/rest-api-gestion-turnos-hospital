6d1e68e28ed66ab988129ea6669e2522d6ce701a
gabrieldiem <gabriel.diem5@gmail.com>
2025-06-07 13:32:08 -0300

    refactor: distribuyo registro del modulo

--- routes/especialidades/routes_especialidades.rb
+++ routes/especialidades/routes_especialidades.rb
@@ -5,6 +5,11 @@ Dir[File.join(__dir__, './', '*.rb')].each { |file| require file }
 
 module RoutesEspecialidades
   def self.registered(app)
+    get_especialidades(app)
+    post_especialidades(app)
+  end
+
+  def self.get_especialidades(app)
     app.get '/especialidades' do
       logger.debug('GET /especialidades')
       especialidades = turnero.obtener_especialidades
@@ -12,7 +17,9 @@ module RoutesEspecialidades
       status 200
       TodasEspecialidadesResponse.new(especialidades).to_json
     end
+  end
 
+  def self.post_especialidades(app)
     app.post '/especialidades' do
       logger.debug("POST /especialidades con params: #{@params}")
       especialidad = turnero.crear_especialidad(@params[:nombre].to_s,
