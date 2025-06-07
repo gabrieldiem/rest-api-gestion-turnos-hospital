83fe8674da7b0e325ea1d6d6550126eb3bd2791f
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-06-07 17:14:04 -0300

    feat-18.0: EstadoTurnos el estado reservado es igual a reservado

--- dominio/estado_turnos.rb
+++ dominio/estado_turnos.rb
@@ -1,1 +1,11 @@
-class EstadoTurnos; end
+class EstadoTurnos
+  attr_reader :valor
+
+  def self.crear_estado_reservado
+    new('reservado')
+  end
+
+  def initialize(valor)
+    @valor = valor
+  end
+end
--- spec/modelo/estado_turnos_spec.rb
+++ spec/modelo/estado_turnos_spec.rb
@@ -2,7 +2,7 @@ require 'integration_helper'
 require_relative '../../dominio/estado_turnos'
 
 describe EstadoTurnos do
-  xit 'El estado reservado es igual a "reservado"' do
+  it 'el estado reservado es igual a "reservado"' do
     estado = described_class.crear_estado_reservado
     expect(estado.valor).to eq('reservado')
   end
