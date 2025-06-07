b24e6bf1e40939919d5d738a0f25c56d79e1663f
gabrieldiem <gabriel.diem5@gmail.com>
2025-06-07 13:40:27 -0300

    refactor: distribuyo registro del modulo

--- routes/medicos/routes_medicos.rb
+++ routes/medicos/routes_medicos.rb
@@ -4,6 +4,14 @@ Dir[File.join(__dir__, '../persistencia', '*.rb')].each { |file| require file }
 
 module RoutesMedicos
   def self.registered(app)
+    get_medicos(app)
+    post_medicos(app)
+    get_turnos_disponibles(app)
+    get_turnos_reservados(app)
+    post_turnos_reservados(app)
+  end
+
+  def self.get_medicos(app)
     app.get '/medicos' do
       logger.debug('GET /medicos')
 
@@ -24,7 +32,9 @@ module RoutesMedicos
         end
       }.to_json
     end
+  end
 
+  def self.post_medicos(app)
     app.post '/medicos' do
       logger.debug("POST /medicos con params: #{@params}")
       medico = turnero.crear_medico(@params[:nombre].to_s,
@@ -41,7 +51,9 @@ module RoutesMedicos
         created_on: medico.created_on
       }.to_json
     end
+  end
 
+  def self.get_turnos_disponibles(app)
     app.get '/medicos/:matricula/turnos-disponibles' do
       logger.debug("POST /medicos/#{params[:matricula]}/turnos-disponibles: #{@params}")
 
@@ -70,27 +82,9 @@ module RoutesMedicos
       status 404
       { mensaje_error: e.message }.to_json
     end
+  end
 
-    app.post '/medicos/:matricula/turnos-reservados' do
-      logger.debug("POST /medicos/#{@params['matricula']}/turnos-reservados con params: #{@params}")
-      turno = turnero.asignar_turno(@params['matricula'], @params[:turno][:fecha].to_s, @params[:turno][:hora].to_s, @params[:dni].to_s)
-      status 201
-      {
-        id: turno.id,
-        matricula: turno.medico.matricula,
-        dni: turno.paciente.dni,
-        turno: {
-          fecha: turno.horario.fecha.to_s,
-          hora: "#{turno.horario.hora.hora}:#{turno.horario.hora.minutos.to_s.rjust(2, '0')}"
-        },
-        created_at: turno.created_on.to_s
-      }.to_json
-    rescue StandardError => e
-      logger.error("Error al reservar con medico: #{e.message}")
-      status 400
-      { mensaje_error: e.message }.to_json
-    end
-
+  def self.get_turnos_reservados(app)
     app.get '/medicos/:matricula/turnos-reservados' do
       logger.debug("GET /medicos/#{params[:matricula]}/turnos-reservados")
 
@@ -116,6 +110,28 @@ module RoutesMedicos
     rescue SinTurnosException => e
       logger.error("Error El médico #{params[:matricula]} no tiene turnos: #{e.message}")
       status 404
+      { mensaje_error: e.message }.to_json
+    end
+  end
+
+  def self.post_turnos_reservados(app)
+    app.post '/medicos/:matricula/turnos-reservados' do
+      logger.debug("POST /medicos/#{@params['matricula']}/turnos-reservados con params: #{@params}")
+      turno = turnero.asignar_turno(@params['matricula'], @params[:turno][:fecha].to_s, @params[:turno][:hora].to_s, @params[:dni].to_s)
+      status 201
+      {
+        id: turno.id,
+        matricula: turno.medico.matricula,
+        dni: turno.paciente.dni,
+        turno: {
+          fecha: turno.horario.fecha.to_s,
+          hora: "#{turno.horario.hora.hora}:#{turno.horario.hora.minutos.to_s.rjust(2, '0')}"
+        },
+        created_at: turno.created_on.to_s
+      }.to_json
+    rescue StandardError => e
+      logger.error("Error al reservar con medico: #{e.message}")
+      status 400
       { mensaje_error: e.message }.to_json
     end
   end
