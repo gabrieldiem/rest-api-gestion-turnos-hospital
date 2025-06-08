3507dcb93bf8ae9e9154758c8ef578b5614b3f65
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-06-08 13:41:46 -0300

    unit-test-18.0: EstadoTurnoFactory podemos obtener el estado reservado a partir del tipo

--- dominio/estado_turnos.rb
+++ dominio/estado_turnos.rb
@@ -9,3 +9,13 @@ class EstadoTurnos
     @valor = valor
   end
 end
+
+class EstadoTurnoReservado
+  ESTADO_RESERVADO = 'reservado'.freeze
+
+  attr_reader :descripcion
+
+  def initialize
+    @descripcion = ESTADO_RESERVADO
+  end
+end
---
+++ spec/modelo/estado_turno_factory_spec.rb
@@ -0,0 +1,10 @@
+require 'integration_helper'
+require_relative '../../dominio/estado_turnos'
+require_relative '../../dominio/estado_turno_factory'
+
+describe EstadoTurnoFactory do
+  xit 'podemos obtener el estado reservado a partir del tipo' do
+    estado = described_class.crear_estado('0')
+    expect(estado.class).to eq(EstadoTurnoReservado)
+  end
+end
