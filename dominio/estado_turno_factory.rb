adc867b7adda1aa78d599536e293918e52b7b8ab
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-06-08 13:44:46 -0300

    feat-18.0: EstadoTurnoFactory podemos obtener el estado reservado a partir del tipo

--- dominio/estado_turno_factory.rb
+++ dominio/estado_turno_factory.rb
@@ -1,1 +1,12 @@
-class EstadoTurnoFactory; end
+require_relative './estado_turnos'
+
+class EstadoTurnoFactory
+  def self.crear_estado(tipo)
+    case tipo
+    when '0'
+      EstadoTurnoReservado.new
+    else
+      raise ArgumentError, "Tipo de estado desconocido: #{tipo}"
+    end
+  end
+end
