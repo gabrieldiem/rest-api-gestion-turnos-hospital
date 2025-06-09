368f04e3532d4b3e05b006aea73d4ef41e8d97f9
Aaron Sanjines <asanjines@fi.uba.ar>
2025-06-09 17:14:28 -0300

    acceptance-test-19.0: declaracion de los tests de aceptacion para feature de recurrencia maxima

---
+++ features/limitacion_recurrencia_maxima.feature
@@ -0,0 +1,28 @@
+#language: es
+Característica: Limitar la asignación de turnos según la recurrencia máxima
+    Como turnero, no quiero otorgar turnos más allá de la recurrencia máxima establecida.
+
+  Antecedentes:
+    Dado el sistema tiene una recurrencia máxima de turnos configurada como 3
+
+  Escenario: 19.1 Asignar un turno cuando el paciente no tiene turnos previos
+    Dado el paciente con DNI "23555102" no tiene turnos asignados
+    Cuando el paciente solicita un turno para la especialidad "Cardiología"
+    Entonces el sistema asigna el turno exitosamente
+
+  Escenario: 19.2 Asignar un turno cuando el paciente no tiene turnos de una especialidad específica
+    Dado el paciente con DNI "23555102" tiene un turno asignado para la especialidad "Dermatología"
+    Cuando el paciente solicita un turno para la especialidad "Cardiología"
+    Entonces el sistema asigna el turno exitosamente
+
+  Escenario: 19.3 Asignar un turno cuando el paciente tiene turnos previos de la especialidad, pero no excede la recurrencia máxima
+    Dado el paciente con DNI "23555102" tiene 2 turnos asignados para la especialidad "Cardiología"
+    Y que "Cardiología" tiene una recurrencia máxima de 3 turnos
+    Cuando el paciente solicita un turno adicional para la especialidad "Cardiología"
+    Entonces el sistema asigna el turno exitosamente
+
+  Escenario: 19.4 Rechazar un turno cuando el paciente excede la recurrencia máxima para la especialidad
+    Dado el paciente con DNI "23555102" tiene 3 turnos asignados para la especialidad "Cardiología"
+    Y que "Cardiología" tiene una recurrencia máxima de 3 turnos
+    Cuando el paciente solicita un turno adicional para la especialidad "Cardiología"
+    Entonces el sistema rechaza el pedido con el mensaje "No puede reservar más turnos para esta especialidad"
---
+++ features/step_definitions/limitacion_recurrencia_maxima_steps.rb
@@ -0,0 +1,38 @@
+Dado('el sistema tiene una recurrencia máxima de turnos configurada como {int}') do |_int|
+  # Dado('el sistema tiene una recurrencia máxima de turnos configurada como {float}') do |float|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('el paciente con DNI {string} no tiene turnos asignados') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Cuando('el paciente solicita un turno para la especialidad {string}') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Entonces('el sistema asigna el turno exitosamente') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('el paciente con DNI {string} tiene un turno asignado para la especialidad {string}') do |_string, _string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('el paciente con DNI {string} tiene {int} turnos asignados para la especialidad {string}') do |_string, _int, _string2|
+  # Dado('el paciente con DNI {string} tiene {float} turnos asignados para la especialidad {string}') do |string, float, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('que {string} tiene una recurrencia máxima de {int} turnos') do |_string, _int|
+  # Dado('que {string} tiene una recurrencia máxima de {float} turnos') do |string, float|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Cuando('el paciente solicita un turno adicional para la especialidad {string}') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Entonces('el sistema rechaza el pedido con el mensaje {string}') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
