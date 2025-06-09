a59cbf59976d12c614d9c21a2985f93855277451
gabrieldiem <gabriel.diem5@gmail.com>
2025-06-08 23:20:33 -0300

    fix-5.3: mejoro stub y hizo Rack::Test:

--- spec/stubs.rb
+++ spec/stubs.rb
@@ -1,17 +1,25 @@
-require_relative './web_mock'
+module FeriadosStubs
+  require 'ostruct'
 
-def cuando_pido_los_feriados(anio, feriados)
-  body = feriados.map do |feriado|
-    {
-      "motivo": 'Es un feriado',
-      "tipo": 'inamovible',
-      "info": '',
-      "dia": feriado.day,
-      "mes": feriado.month,
-      "id": 'un-feriado'
-    }
+  def cuando_pido_los_feriados(anio, feriados)
+    body = []
+    feriados.each do |feriado|
+      feriado = {
+        motivo: 'Es un feriado',
+        tipo: 'inamovible',
+        info: '',
+        dia: feriado.day,
+        mes: feriado.month,
+        id: 'un-feriado'
+      }
+      body.push(feriado)
+    end
+
+    response = OpenStruct.new(status: 200, body: body.to_json)
+
+    allow(Faraday).to receive(:get).and_call_original
+    allow(Faraday).to receive(:get)
+      .with("#{ENV['API_FERIADOS_URL']}/#{anio}")
+      .and_return(response)
   end
-
-  stub_request(:any, "#{ENV['API_FERIADOS_URL']}/#{anio}")
-    .to_return(body: body.to_json, status: 200)
 end
