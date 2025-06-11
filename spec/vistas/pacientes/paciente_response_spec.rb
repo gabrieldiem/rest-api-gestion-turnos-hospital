92ded2389e973c824a556b1492211efc4f668632
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-06-11 16:49:00 -0300

    unit-test-10.1: La vista del paciente debe devolver la reputacion

--- spec/vistas/pacientes/paciente_response_spec.rb
+++ spec/vistas/pacientes/paciente_response_spec.rb
@@ -2,13 +2,14 @@ require 'integration_helper'
 require_relative '../../../vistas/pacientes/paciente_response'
 
 describe PacienteResponse do
-  it 'transforma exitosamente a JSON' do
+  xit 'transforma exitosamente a JSON' do
     paciente = Paciente.new('roberto@mail.com', '12345', 'robertito', 1)
     response = described_class.new(paciente).to_json
     expect(response).to eq({
       username: paciente.username,
       dni: paciente.dni,
-      email: paciente.email
+      email: paciente.email,
+      reputacion: paciente.reputacion,
     }.to_json)
   end
 end
