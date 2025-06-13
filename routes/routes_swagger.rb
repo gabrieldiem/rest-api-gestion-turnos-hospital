b8f7e997dd6148714f4c13c3a27d9dbaa3248756
gabrieldiem <gabriel.diem5@gmail.com>
2025-06-13 12:48:41 -0300

    refactor: separo funciones de HTTP de swagger en el register

--- routes/routes_swagger.rb
+++ routes/routes_swagger.rb
@@ -1,10 +1,17 @@
 module RoutesSwagger
   def self.registered(app)
+    get_openapi_json(app)
+    get_swagger_ui(app)
+  end
+
+  def self.get_openapi_json(app)
     app.get '/openapi.json' do
       status 200
       send_file File.join(__dir__, '../openapi.json')
     end
+  end
 
+  def self.get_swagger_ui(app)
     app.get '/swagger' do
       content_type 'text/html'
       status 200
