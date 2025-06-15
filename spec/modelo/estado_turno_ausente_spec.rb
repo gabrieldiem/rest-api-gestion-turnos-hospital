2f7636be4fd58c9fb87bd90523ba0b7a5f386fd8
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-06-15 20:53:24 -0300

    unit-test-25.1: EstadoTurnoAusente el estado ausente cuando cambiamos de asistencia a true es igual a presente

--- spec/modelo/estado_turno_ausente_spec.rb
+++ spec/modelo/estado_turno_ausente_spec.rb
@@ -6,4 +6,10 @@ describe EstadoTurnoAusente do
     estado = described_class.new
     expect(estado.descripcion).to eq('ausente')
   end
+
+  xit 'el estado ausente cuando cambiamos de asistencia a true es igual a "presente"' do
+    estado = described_class.new
+    nuevo_estado = estado.cambiar_asistencia(true)
+    expect(nuevo_estado.descripcion).to eq('presente')
+  end
 end
