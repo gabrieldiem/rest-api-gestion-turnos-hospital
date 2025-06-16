ec22db175a95b7d6132472d18e5c74ff6d9e3b96
sev <santisevitz@hotmail.com>
2025-06-16 00:24:41 -0300

    unit-test: paciente devuelve una lista de solo turnos reservados

--- spec/modelo/paciente_spec.rb
+++ spec/modelo/paciente_spec.rb
@@ -381,8 +381,8 @@ describe Paciente do
       paciente = described_class.new('juan.perez@example.com', '12345678', '@juanperez', 1)
       medico = Medico.new('Juan', 'Perez', 'NAC123', especialidad)
       turno1 = Turno.new(paciente, medico, Horario.new(Date.today + 1, Hora.new(10, 0)))
-      turno2 = Turno.new(paciente, medico, Horario.new(Date.today + 1, Hora.new(10, 0)))
-      turno_reservado = Turno.new(paciente, medico, Horario.new(Date.today + 1, Hora.new(10, 0)))
+      turno2 = Turno.new(paciente, medico, Horario.new(Date.today + 2, Hora.new(10, 0)))
+      turno_reservado = Turno.new(paciente, medico, Horario.new(Date.today + 3, Hora.new(10, 0)))
 
       turno1.cambiar_asistencia(true)
       turno2.cambiar_asistencia(false)
@@ -408,7 +408,7 @@ describe Paciente do
       paciente = described_class.new('juan.perez@example.com', '12345678', '@juanperez', 1)
       medico = Medico.new('Juan', 'Perez', 'NAC123', especialidad)
       turno1 = Turno.new(paciente, medico, Horario.new(Date.today + 1, Hora.new(10, 0)))
-      turno2 = Turno.new(paciente, medico, Horario.new(Date.today + 1, Hora.new(10, 0)))
+      turno2 = Turno.new(paciente, medico, Horario.new(Date.today + 2, Hora.new(10, 0)))
 
       turno1.cambiar_asistencia(true)
       turno2.cambiar_asistencia(false)
@@ -419,7 +419,24 @@ describe Paciente do
       expect(paciente.obtener_turnos_reservados.size).to eq(0)
     end
 
-    xit 'devuelve una lista de solo turnos reservados' do
+    it 'devuelve una lista de solo turnos reservados' do
+            paciente = described_class.new('juan.perez@example.com', '12345678', '@juanperez', 1)
+      medico = Medico.new('Juan', 'Perez', 'NAC123', especialidad)
+      turno1 = Turno.new(paciente, medico, Horario.new(Date.today + 1, Hora.new(10, 0)))
+      turno2 = Turno.new(paciente, medico, Horario.new(Date.today + 2, Hora.new(10, 0)))
+      turno3 = Turno.new(paciente, medico, Horario.new(Date.today + 3, Hora.new(10, 0)))
+
+      turno1.cambiar_asistencia(true)
+      turno2.cambiar_asistencia(false)
+
+      paciente.turnos_reservados << turno1
+      paciente.turnos_reservados << turno2
+      paciente.turnos_reservados << turno3
+
+      expect(paciente.obtener_turnos_reservados.size).to eq(1)
+      expect(paciente.obtener_turnos_reservados).to include(turno3)
+      expect(paciente.obtener_turnos_reservados).not_to include(turno1)
+      expect(paciente.obtener_turnos_reservados).not_to include(turno2)
     end
   end
 end
