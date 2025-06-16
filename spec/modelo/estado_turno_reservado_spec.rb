1fb3f85decc89172dfcb03d47fa0701d638d36f5
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-06-15 21:19:42 -0300

    unit-test-25.1: EstadoTurnoReservado el estado reservado acepta el metodo reservado? y asistio?

--- spec/modelo/estado_turno_reservado_spec.rb
+++ spec/modelo/estado_turno_reservado_spec.rb
@@ -18,4 +18,10 @@ describe EstadoTurnoReservado do
     nuevo_estado = estado.cambiar_asistencia(false)
     expect(nuevo_estado.descripcion).to eq('ausente')
   end
+
+  xit 'el estado reservado acepta el metodo reservado? y asistio?' do
+    estado = described_class.new
+    expect(estado.reservado?).to be true
+    expect(estado.asistio?).to be false
+  end
 end
