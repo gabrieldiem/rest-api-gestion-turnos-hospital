a38bc146fe4ea0834f46f04ce62813ac8c495c9b
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-06-18 17:24:30 -0300

    feat-25.1: CalendarioDeTurnos salta los horarios en días feriados

--- dominio/calendario_de_turnos.rb
+++ dominio/calendario_de_turnos.rb
@@ -6,9 +6,10 @@ class CalendarioDeTurnos
   DOMINGO = 'Sunday'.freeze
   FIN_DE_SEMANA = [SABADO, DOMINGO].freeze
 
-  def initialize(proveedor_de_fecha, proveedor_de_hora, hora_de_comienzo_de_jornada, hora_de_fin_de_jornada)
+  def initialize(proveedor_de_fecha, proveedor_de_hora, proveedor_de_feriados, hora_de_comienzo_de_jornada, hora_de_fin_de_jornada)
     @proveedor_de_fecha = proveedor_de_fecha
     @proveedor_de_hora = proveedor_de_hora
+    @proveedor_de_feriados = proveedor_de_feriados
     @hora_de_comienzo_de_jornada = hora_de_comienzo_de_jornada
     @hora_de_fin_de_jornada = hora_de_fin_de_jornada
   end
@@ -41,9 +42,10 @@ class CalendarioDeTurnos
 
   def calcular_siguiente_horario_disponible(fecha_actual, indice_horario, duracion_turno, turnos_ya_asignados)
     horario = calcular_siguiente_horario(fecha_actual, indice_horario, duracion_turno)
+    feriados = @proveedor_de_feriados.obtener_feriados(horario.fecha.year)
 
     while horario.hora.hora < @hora_de_fin_de_jornada.hora
-      return horario if esta_disponible?(horario, turnos_ya_asignados, nil)
+      return horario if esta_disponible?(horario, turnos_ya_asignados, feriados)
 
       indice_horario += 1
       horario = calcular_siguiente_horario(fecha_actual, indice_horario, duracion_turno)
@@ -54,13 +56,18 @@ class CalendarioDeTurnos
 
   private
 
-  def esta_disponible?(horario, turnos_ya_asignados, _feriados)
+  def esta_disponible?(horario, turnos_ya_asignados, feriados)
     !existe_turno_asignado?(horario, turnos_ya_asignados) &&
+      !es_dia_feriado?(horario, feriados) &&
       !es_fin_de_semana?(horario)
   end
 
   def existe_turno_asignado?(horario_a_verificar, turnos_ya_asignados)
     turnos_ya_asignados.any? { |turno| turno.horario == horario_a_verificar }
+  end
+
+  def es_dia_feriado?(horario_a_verificar, feriados)
+    feriados.any? { |feriado| feriado.fecha == horario_a_verificar.fecha }
   end
 
   def es_fin_de_semana?(horario_a_verificar)
--- spec/modelo/calendario_de_turnos_spec.rb
+++ spec/modelo/calendario_de_turnos_spec.rb
@@ -2,14 +2,37 @@ require 'rspec'
 require_relative '../../dominio/calendario_de_turnos'
 require_relative '../../lib/horario'
 require_relative '../../lib/hora'
+require_relative '../stubs'
+
+def cuando_pido_los_feriados(anio, feriados)
+  body = []
+  feriados.each do |feriado|
+    feriado = {
+      motivo: 'Es un feriado',
+      tipo: 'inamovible',
+      info: '',
+      dia: feriado.day,
+      mes: feriado.month,
+      id: 'un-feriado'
+    }
+    body.push(feriado)
+  end
+
+  response = OpenStruct.new(status: 200, body: body.to_json)
+
+  allow(Faraday).to receive(:get).and_call_original
+  allow(Faraday).to receive(:get)
+    .with("#{ENV['API_FERIADOS_URL']}/#{anio}")
+    .and_return(response)
+end
 
 describe CalendarioDeTurnos do
   let(:fecha_de_hoy) { Date.new(2025, 1, 1) }
-  let(:hora_actual) { DateTime.new(2025, 1, 1, 8, 0) }
-  let(:hora_de_comienzo_de_jornada) { Hora.new(8, 0) }
-  let(:hora_de_fin_de_jornada) { Hora.new(17, 0) }
-  let(:turnos_asignados) { [] }
-
+  let(:api_feriados_url) { ENV['API_FERIADOS_URL'] }
+  let(:logger) do
+    SemanticLogger.default_level = :fatal
+    Configuration.logger
+  end
   let(:proveedor_de_fecha) do
     proveedor_double = class_double(Date, today: fecha_de_hoy)
     ProveedorDeFecha.new(proveedor_double)
@@ -18,9 +41,20 @@ describe CalendarioDeTurnos do
     proveedor_double = class_double(Time, now: hora_actual)
     ProveedorDeHora.new(proveedor_double)
   end
-
+  let(:proveedor_de_feriados) do
+    ProveedorDeFeriados.new(api_feriados_url, logger)
+  end
   let(:calendario) do
-    described_class.new(proveedor_de_fecha, proveedor_de_hora, hora_de_comienzo_de_jornada, hora_de_fin_de_jornada)
+    described_class.new(proveedor_de_fecha, proveedor_de_hora, proveedor_de_feriados, hora_de_comienzo_de_jornada, hora_de_fin_de_jornada)
+  end
+  let(:hora_actual) { DateTime.new(2025, 1, 1, 8, 0) }
+  let(:hora_de_comienzo_de_jornada) { Hora.new(8, 0) }
+  let(:hora_de_fin_de_jornada) { Hora.new(17, 0) }
+  let(:turnos_asignados) { [] }
+
+  before(:each) do
+    ENV['API_FERIADOS_URL'] = 'http://www.feriados-url.com'
+    cuando_pido_los_feriados(2025, [])
   end
 
   it 'obtengo la fecha y la hora actual el proveedor de fecha' do
@@ -93,8 +127,7 @@ describe CalendarioDeTurnos do
     expect(resultado).to eq(Horario.new(fecha_de_hoy, Hora.new(9, 0)))
   end
 
-  xit 'salta los horarios en días feriados' do
-    allow(proveedor_feriados).to receive(:obtener_feriados).and_return(Date.new(2025, 6, 20))
+  it 'salta los horarios en días feriados' do
     allow(calendario).to receive(:calcular_siguiente_horario).and_return(
       Horario.new(Date.new(2025, 6, 20), Hora.new(9, 0)), Horario.new(Date.new(2025, 6, 20), Hora.new(9, 0))
     )
