dd03c06831a928e8d36a635d5b9ba0a3c7352458
sev <santisevitz@hotmail.com>
2025-06-18 02:04:55 -0300

    feat: agregando proveedor de fecha fijo para poder cambiar la fecha para el ambiente de test

---
+++ spec/lib/proveedor_de_fecha_fijo_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require 'date'
+require_relative '../../lib/proveedor_de_fecha_fijo'
+
+RSpec.describe ProveedorDeFechaFijo do
+  it 'devuelve la fecha fija pasada en el constructor' do
+    fecha = Date.new(2030, 5, 15)
+    proveedor = ProveedorDeFechaFijo.new(fecha)
+    expect(proveedor.hoy).to eq(fecha)
+  end
+
+  it 'devuelve la fecha como Date aunque se pase un string' do
+    proveedor = ProveedorDeFechaFijo.new('2022-12-31')
+    expect(proveedor.hoy).to eq(Date.new(2022, 12, 31))
+  end
+
+  it 'devuelve la fecha como Date aunque se pase un DateTime' do
+    proveedor = ProveedorDeFechaFijo.new(DateTime.new(2021, 1, 1, 10, 0, 0))
+    expect(proveedor.hoy).to eq(Date.new(2021, 1, 1))
+  end
+end
